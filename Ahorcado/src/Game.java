7b075fc567704a7afebe67ddb2299f250a71e9b5
Alejo12680 <alejo152725@gmail.com>
2025-01-31 10:39:39 -0500

    feat: Actualiza la primera plantilla del juego

--- Ahorcado/src/Game.java
+++ Ahorcado/src/Game.java
@@ -3,5 +3,21 @@ import java.util.Scanner;
 public class Game {
     public static void main(String[] args) throws Exception {
         System.out.println("Welcome to the game!");
+        // clase Scanner para leer la entrada del usuario
+        Scanner scanner = new Scanner(System.in);
+
+        // variables
+        String secretWordString = "hello";
+        int attemptsMax = 10;
+        int attempts = 0;
+        boolean isGameOver = false;
+
+        // Array de caracteres para la palabra secreta 
+        char[] charSecretWord = new char[secretWordString.length()];
+        
+        for (int i = 0; i < charSecretWord.length; i++) {
+            charSecretWord[i] = '_';
+            System.out.print(charSecretWord[i]);
+        }
     }
 }
