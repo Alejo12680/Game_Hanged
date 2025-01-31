3df072d57d66246a388099b861779a4c2a59112c
Alejo12680 <alejo152725@gmail.com>
2025-01-31 10:55:00 -0500

    feat: Actualiza las estructuras de control para tener un flujo cuando el usuario interactue con el game.

--- Ahorcado/src/Game.java
+++ Ahorcado/src/Game.java
@@ -3,10 +3,10 @@ import java.util.Scanner;
 public class Game {
     public static void main(String[] args) throws Exception {
         System.out.println("Welcome to the game!");
-        // clase Scanner para leer la entrada del usuario
+        // Clase Scanner para leer la entrada del usuario
         Scanner scanner = new Scanner(System.in);
 
-        // variables
+        // Variables
         String secretWordString = "hello";
         int attemptsMax = 10;
         int attempts = 0;
@@ -15,9 +15,28 @@ public class Game {
         // Array de caracteres para la palabra secreta 
         char[] charSecretWord = new char[secretWordString.length()];
         
+        // Estructura de control iterativa para mostrar la palabra secreta en lineas (Bucle)
         for (int i = 0; i < charSecretWord.length; i++) {
             charSecretWord[i] = '_';
-            System.out.print(charSecretWord[i]);
+        }
+
+        // Estructura de control iterativa para preguntar al usuario por una letra (Bucle)
+        while (attempts < attemptsMax && !isGameOver) {
+            System.out.println("Palabra a Adivinar: " + String.valueOf(charSecretWord));
+            System.out.println("Introduce una letra por favor: ");
+            // Leer la letra ingresada por el usuario y si ingresa más de una letra solo se toma la primera
+            char letter = scanner.next().charAt(0);
+
+            // Estructura de control iterativa para comparar la letra ingresada con la palabra secreta (Bucle)
+            boolean isLetterInWord = false;
+            for (int i = 0; i < secretWordString.length(); i++) {
+                // Estructura de control condicional para verificar si la letra ingresada esta en la palabra secreta
+                if (secretWordString.charAt(i) == letter) {
+                    charSecretWord[i] = letter;
+                    isLetterInWord = true;
+                }
+            }
+            
         }
     }
 }
